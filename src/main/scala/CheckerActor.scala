03b1e75c0a4e7ee6def27c543d7d5bc67988fbf1
Hakimba <hakim.baaloudj@gmail.com>
2021-02-07 02:04:00 +0100

    état actuel du projet

---
+++ src/main/scala/CheckerActor.scala
@@ -0,0 +1,61 @@
+package upmc.akka.leader
+
+import java.util
+import java.util.Date
+
+import akka.actor._
+
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+abstract class Tick
+case class CheckerTick () extends Tick
+
+class CheckerActor (val id:Int, val terminaux:List[Terminal], electionActor:ActorRef) extends Actor {
+
+     var time : Int = 200
+     val father = context.parent
+
+     var nodesAlive:List[Int] = List()
+     var datesForChecking:List[Date] = List()
+     var lastDate:Date = null
+
+     var leader : Int = -1
+
+    def receive = {
+
+         // Initialisation
+        case Start => {
+             context.system.scheduler.schedule(0 milliseconds, 100 milliseconds, self, CheckerTick)
+        }
+
+        // A chaque fois qu'on recoit un Beat : on met a jour la liste des nodes
+        case IsAlive (nodeId) => {
+             nodesAlive.find(_.==(nodeId)) match {
+                  case None => {
+                       nodesAlive = nodeId :: nodesAlive
+                  }
+                  case _ =>
+             }
+             println(nodesAlive)
+        }
+
+        case IsAliveLeader (nodeId) => if (leader != nodeId) leader = nodeId 
+
+        // A chaque fois qu'on recoit un CheckerTick : on verifie qui est mort ou pas
+        // Objectif : lancer l'election si le leader est mort
+        case CheckerTick => {
+             if (leader != -1){
+                  nodesAlive.find(_.==(leader)) match {
+                  case None => {
+                       electionActor ! StartWithNodeList (nodesAlive)
+                       println("election started")
+                  }
+                  case _ => {println("leader vivant")}
+
+               }
+             }
+        }
+
+    }
+}
---
+++ src/main/scala/ElectionActor.scala
@@ -0,0 +1,113 @@
+package upmc.akka.leader
+
+import akka.actor._
+
+abstract class NodeStatus
+case class Passive () extends NodeStatus
+case class Candidate () extends NodeStatus
+case class Dummy () extends NodeStatus
+case class Waiting () extends NodeStatus
+case class Leader () extends NodeStatus
+
+abstract class LeaderAlgoMessage
+case class Initiate () extends LeaderAlgoMessage
+case class ALG (list:List[Int], nodeId:Int) extends LeaderAlgoMessage
+case class AVS (list:List[Int], nodeId:Int) extends LeaderAlgoMessage
+case class AVSRSP (list:List[Int], nodeId:Int) extends LeaderAlgoMessage
+
+case class StartWithNodeList (list:List[Int])
+
+class ElectionActor (val id:Int, val terminaux:List[Terminal]) extends Actor {
+
+     val father = context.parent
+     var nodesAlive:List[Int] = List(id)
+
+     var candSucc:Int = -1
+     var candPred:Int = -1
+     var status:NodeStatus = new Passive ()
+
+// assez sale pour l'instant niveau gestion d'erreurs
+     def getActorById (id:Int):ActorSelection = {
+          var remote:ActorSelection =  null
+          if (id < nodesAlive.length){
+               terminaux.foreach(n => {
+                    if (n.id == id) {
+                         remote = context.actorSelection("akka.tcp://LeaderSystem" + n.id + "@" + n.ip + ":" + n.port + "/user/Node")
+                    }
+               })
+          } else {
+               println("Id > length of the list of Actors")
+          }
+          
+          return remote
+     }
+
+     def getNeigh (id:Int) : ActorSelection = {
+          var remote:ActorSelection = null
+          if (id >= nodesAlive.length){
+               remote = getActorById(0)
+          } else {
+               remote = getActorById(id)
+          }
+          return remote
+     }
+
+     def receive = {
+          // Initialisation
+          case Start => {
+               self ! Initiate
+          }
+
+          case StartWithNodeList (list) => {
+               if (list.isEmpty) {
+                    this.nodesAlive = this.nodesAlive:::List(id)
+               }
+               else {
+                    this.nodesAlive = list
+               }
+
+               // Debut de l'algorithme d'election
+               self ! Initiate
+          }
+
+          case Initiate => {
+               val r = getNeigh(this.id)
+               r ! ALG (nodesAlive,this.id)
+          }
+
+          case ALG (list, init) => {
+               status match {
+                    case Passive() => {
+                         status = new Dummy ()
+                         val r = getNeigh(this.id)
+                         r ! ALG (nodesAlive,init)
+                    }
+                    case Candidate() => {
+                         candPred = init
+                         this.id match {
+                              case a if a > init => {
+                                   if(candSucc == -1) {
+                                        status = new Waiting ()
+                                        val r = getActorById(init)
+                                        r ! AVS (nodesAlive,this.id)
+                                   } else {
+                                        val r = getActorById(candSucc)
+                                        r ! AVSRSP (nodesAlive,candPred)
+                                        status = new Dummy ()
+                                   }
+                              }
+                              case b if b == init => {
+                                   status = new Leader ()
+                              }
+                         }
+                    }
+                    case _ =>
+               }
+          }
+
+          case AVS (list, j) => 
+
+          case AVSRSP (list, k) => 
+
+     }
+}
